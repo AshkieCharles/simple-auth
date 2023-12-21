79697d6f32ba8b706a62b4939e93db07cdc77b2e
Ashkie <ashkiecharles@gmail.com>
2023-12-21 13:20:37 -0700

    added simple auth

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,14 +1,27 @@
+<script>
+  export let form;
+  console.log(form)
+</script>
+
 <div class="container">
   <h2>Login</h2>
-
-  <form method="POST">
+  {#if form?.message}
+    <p class="red">{form.message}</p>
+  {/if}
+  <form method="post">
     <div>
-      <input type="email" name="email"  value="example@gmail.com">
+      <input type="email" name="email"  value={form?.email || ""} >
     </div>
     <div>
-      <input type="password" name="password" value="password">
+      <input type="password" name="password">
     </div>
     <button>Login</button>
 
   </form>
 </div>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>

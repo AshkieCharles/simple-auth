bf6366f11caac6316bd1d6f06a4986af3ba3a6f0
Ashkie <ashkiecharles@gmail.com>
2023-12-21 12:58:37 -0700

    added forms

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,14 @@
+<div class="container">
+  <h2>Login</h2>
+
+  <form method="POST">
+    <div>
+      <input type="email" name="email"  value="example@gmail.com">
+    </div>
+    <div>
+      <input type="password" name="password" value="password">
+    </div>
+    <button>Login</button>
+
+  </form>
+</div>
